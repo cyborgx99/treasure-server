6f6c509bd0b9e5eac7dba1d2c2c65d45805d652d
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-09-01 12:30:32 +0200

    Add jwt strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from '../auth.service';
+import { JwtPayload, User } from '../dto/auth.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { id } = payload;
+
+    const user = this.authService.findUserById(id);
+
+    if (!user) {
+      throw new UnauthorizedException('Unathorized');
+    }
+
+    return user;
+  }
+}
