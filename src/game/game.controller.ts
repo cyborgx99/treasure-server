bc2461d46d40c1e4011ad17e0068b3ef8e8f7f65
Sergey Kalashnykov <cyborgx999@gmail.com>
2022-09-02 02:14:59 +0200

    implement game endpoints

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { User } from 'src/auth/dto/auth.dto';
+import {
+  GetScoresResponse,
+  RevealTilesDto,
+  RevealTilesResponse,
+  StartGameResponse,
+} from './dto/game.dto';
 import { GameService } from './game.service';
 
 @UseGuards(AuthGuard('jwt'))
@@ -8,7 +16,24 @@ export class GameController {
   constructor(private gameService: GameService) {}
 
   @Get('start')
-  startGame() {
-    return this.gameService.startGame();
+  startGame(@Req() request: Request): StartGameResponse {
+    const user = <User>request.user;
+    return this.gameService.startGame(user.id);
+  }
+
+  @Post('reveal')
+  revealTiles(@Body() tilesToReveal: RevealTilesDto): RevealTilesResponse {
+    return this.gameService.revealTiles(tilesToReveal);
+  }
+
+  @Get('scoreboard')
+  getScores(): GetScoresResponse {
+    return this.gameService.getScores();
+  }
+
+  @Get('current')
+  getCurrentGame(@Req() request: Request): StartGameResponse {
+    const user = <User>request.user;
+    return this.gameService.getCurrentGame(user.id);
   }
 }
